2b989fe8a3d29dc13271e2c1b6c015212b953d2b
Jinseok EO <39117390+Eoseok@users.noreply.github.com>
2023-10-08 15:22:31 +0900

    Update with .env settings

--- jse_project_setup.m
+++ jse_project_setup.m
@@ -1,4 +1,4 @@
-function [masterPath,dataPath] = jse_project_setup()
+function jse_project_setup()
 % JSE_PROJECT_SETUP is template setup file for project MATLAB based code
 % folder. If the project is performed in various environments, you can
 % proceed with setting by executing this code. If .gitignore exist, we
@@ -11,122 +11,108 @@ function [masterPath,dataPath] = jse_project_setup()
 %   2. Check pathdef.m permission
 %   3. Remove old project path
 %   4. Add and save new project path
+%   5. Load environment file and set to GLOBAL (Project specification need)
 %   ------------------------    Custom Section    ------------------------
-%   5. Setting masterPath & dataPath 
-%   6. Check required software path 
-%   7. +++
+%   A. Check additional toolbox path
 %   ----------------------------------------------------------------------
-%
-%   # This code referenced VBA_setup.m from VBA-toolbox.
-%     (https://mbb-team.github.io/VBA-toolbox)
-%   
+%_________________________________________________________________________
 %   $ Dec 14 2021, Jinseok Eo , urjinseok@gmail.com
+%   $ OCT  8 2023, Add environment setting and modify addpath, Jinseok Eo
 
-%%  1. Code path check
-%   ----------------------------------------------------------------------
-mfilePath   = fileparts(mfilename('fullpath'));
-[~,pn]      = fileparts(mfilePath);
-currentPath = pwd;
+%   ======================================================================
+%                             Mandatory Section
+%   ======================================================================
+%   1. Code path check
+%   ======================================================================
+    mfilePath   = fileparts(mfilename('fullpath'));
+    [~,pn]      = fileparts(mfilePath);
+    currentPath = pwd;
 
 if  ~(strcmpi(mfilePath,pwd))
     cd(mfilePath);
 end
 
-%%  2. Check pathdef.m permission
-%   ----------------------------------------------------------------------
-err = savepath;
-
-if err
-    % warn user the new path wont be saved:
-    warning(['savepath() failed. ' ...
-        'Installation will continue but your changes will not be saved.']);
+%   2. Check pathdef.m permission
+%   ======================================================================
+    err = savepath;
+if  err
+    warning(['savepath() failed. '         ...
+             'Installation will continue ' ...
+             'but your changes will not be saved.']);
 end
 
-%%  3. Remove old project path 
-%   ----------------------------------------------------------------------
-%   Check Operation system
-isWin   = (~isempty(contains(computer, 'PCWIN')) || ...
-                                      strcmp(computer, 'i686-pc-mingw32'));
-isOSX   = (~isempty(contains(computer, 'MAC'))   || ... 
-                             ~isempty(contains(computer, 'apple-darwin')));
-isLinux = (strcmp(computer,'GLNX86') || strcmp(computer,'GLNXA64') ||...
-                                ~isempty(contains(computer, 'linux-gnu')));
-
-if isOSX || isLinux
-    p = strsplit(path,':');
-elseif isWin
-    p = strsplit(path,';');
-end
-
-r = regexp(p, ['^.*' pn '.*$'],'match');
-r = r(~cell2mat(cellfun(@isempty,r,'UniformOutput',false)));
-
-if  ~(isempty(r))
-    for iP = 1:numel(r)
-        rmpath(char(r{iP}))
+%   3. Add project path
+%   ======================================================================
+    isWin   = (~isempty(contains(computer, 'PCWIN')) ||...
+                          strcmp(computer, 'i686-pc-mingw32'));
+    isOSX   = (~isempty(contains(computer, 'MAC'))   ||... 
+               ~isempty(contains(computer, 'apple-darwin')));
+    isLinux = (~isempty(contains(computer, 'linux-gnu')) ||...
+               strcmp(computer,'GLNX86')                 ||...
+               strcmp(computer,'GLNXA64'));
+    
+    if isOSX || isLinux
+        p = strsplit(path,':');
+    elseif isWin
+        p = strsplit(path,';');
     end
-    try
-        savepath
-    catch
-
+    
+    r = regexp(p, ['^.*' pn '.*$'],'match');
+    r = r(~cell2mat(cellfun(@isempty,r,'UniformOutput',false)));
+    if  ~(isempty(r))
+        for iP = 1:numel(r)
+            rmpath(char(r{iP}))
+        end
+       savepath
     end
-end
-%%  4.Add and save new project path
-%   ----------------------------------------------------------------------
-p        = strsplit(genpath(pwd),':');
-ref_path = ['refs' filesep 'heads'];
-r        = regexp(p,['^.*' pn filesep '.git' filesep ...
-                                           '(?!' ref_path ').*$'],'match');
-p = p(cell2mat(cellfun(@isempty,r,'UniformOutput',false)));
 
-for iP = 1:numel(p)
-    addpath(char(p{iP}))
-end
+%   4. Add and save new project path
+%   ======================================================================
+    p        = strsplit(genpath(pwd),':');
+    ref_path = ['refs' filesep 'heads'];
+    r        = regexp(p,['^.*' pn filesep '.git' filesep ...
+                         '(?!' ref_path ').*$'],'match');
+    p = p(cell2mat(cellfun(@isempty,r,'UniformOutput',false)));
+    
+    for iP = 1:numel(p)
+        if ~(ismember(p{iP},strsplit(path,pathsep)))
+            addpath(char(p{iP}))
+        end
+    end
 
 %   Remove .gitignore
-gitig    = [pwd filesep '.gitignore'];
-if  exist(gitig,'file')
-    fid   = fopen(gitig);
-    tline = fgetl(fid);
-    warning off
-    while ischar(tline)
-        try
-            rmpath(tline);
-        catch
-        end
+%   ----------------------------------------------------------------------
+    gitig    = [pwd filesep '.gitignore'];
+    if  exist(gitig,'file')
+        fid   = fopen(gitig);
         tline = fgetl(fid);
+        warning off
+        while ischar(tline)
+            try
+                rmpath(tline);
+            catch
+            end
+            tline = fgetl(fid);
+        end
+        warning on 
     end
-    warning on 
-end
 
-try
     savepath
-catch
 
-end
+%   5. Load environment file and set to GLOBAL (Project Specification)
+%   ======================================================================
+%   Modify by Project
+    global MyProject_Env
+    projectEnv    = 'MyProject.env';
+    MyProject_Env = loadenv(projectEnv);
 
-%%  5. Setting masterPath and dataPath
-%   ----------------------------------------------------------------------
+%   ======================================================================
+%                               Custom Section   
+%   ======================================================================
+%   A. Check additional toolbox path
+%   ======================================================================
 
 
-%%  6. Check required softwarepath
-%   ----------------------------------------------------------------------
-%   SPM
-try
-    p = fileparts(which('spm'));
-    fprintf('Required software : SPM is installed in %s\n',p);
-catch
-    fprintf('Please add SPM in your MATLAB path\n');
-end
-
-%   mnet
-try
-    p = fileparts(which('mnet'));
-    fprintf('Required software : mnet is installed in %s\n',p);
-catch
-    fprintf('Please add mnet in your MATLAB path\n');
-end
-
-%%  Return to folder
-%   ----------------------------------------------------------------------
-cd(currentPath);
+%__________________________________________________________________________
+%   Get Back
+    cd(currentPath);
